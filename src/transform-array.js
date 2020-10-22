e2b4363f8f60b619d0854444fe66b92d5bce5576
Oleh Leochko <olegleochko7@gmail.com>
2020-10-23 00:00:40 +0300

    add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,35 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
   let res = [];
-  if (Array.isArray(arr) === false) {
-    throw new Error();
-  }
-  //arr.forEach((item, index) => res.push(item));
+
   for (let i = 0; i < arr.length; i++) {
-    res.push(arr[i]);
-  }
-  for (let i = 0; i < res.length; i++) {
-    if (res[i] === "--double-next") {
-      res[i] = res[i + 1];
-    }
-    if (res[i] === "--double-prev") {
-      if (res[i - 1]) {
-        res[i] = res[i - 1];
-      } else {
-        res.splice(0, 1);
-      }
-    }
-    if (res[i] === "--discard-next") {
-      res.splice(i, 2);
-    }
-    if (res[i] === "--discard-prev") {
-      if (res[i - 1]) {
-        res.splice(i - 1, 2);
-      } else {
-        res.splice(i, 1);
-        res[res.length] = res.length + 1;
-      }
+
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (res.length > 0 && arr[i - 2] !== '--discard-next') {
+          res.pop();
+        }
+        break;
+      case '--double-next':
+        if (i !== arr.length - 1) {
+          res.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          res.push(arr[i - 1]);
+        }
+        break;
+      default:
+        res.push(arr[i]);
     }
   }
   return res;
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,106 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError("Not implemented");
-    // remove line with error and write your code here
+  encrypt(message, gama) {
+    //let alphabeter = ["А", "Б", "В", "Г", "Ґ", "Д", "Е", "Є", "Ж", "З", "И", "І", "Ї", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ь", "Ю", "Я", "_", "0", "1", "2", "3", "5", "6", "7", "8", "9",];
+    // let gama = "БРАТИСЛАВ";
+    // let message = "НЕРЮНГРІ_230_КМ";
+    let alphabeter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    gama = gama.split('');
+    message = message.split('');
+    let sum = 1;
+    for (let i = 0; i < message.length; i++) {
+      if (sum <= gama.length) {
+        gama[i] = gama[sum - 1]
+      }
+      else {
+        sum = sum - gama.length;
+        gama[i] = gama[sum - 1];
+      }
+      sum++;
+    }
+    let messageNumOfSimbol = [];
+    let gamaNumOfSimbol = [];
+    let sumOfSimbol = [];
+    for (let i = 0; i < message.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (message[i] == alphabeter[j]) {
+          messageNumOfSimbol[i] = j
+        };
+      }
+    }
+    for (let i = 0; i < gama.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (gama[i] == alphabeter[j]) {
+          gamaNumOfSimbol[i] = j
+        };
+      }
+    }
+    for (let i = 0; i < message.length; i++) {
+      sumOfSimbol[i] = (gamaNumOfSimbol[i] + 1) +
+        (messageNumOfSimbol[i] + 1);
+      if (sumOfSimbol[i] > alphabeter.length)
+        sumOfSimbol[i] = sumOfSimbol[i] - alphabeter.length;
+    }
+
+    let newmessage = new Array(message.length);
+    for (let i = 0; i < message.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (sumOfSimbol[i] == j + 1)
+          newmessage[i] = alphabeter[j];
+      }
+    }
+
+    return newmessage.join("");
   }
-  decrypt() {
-    throw new CustomError("Not implemented");
-    // remove line with error and write your code here
+  decrypt(newmessage, gama) {
+    let alphabeter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    gama = Array.from(gama);
+    let message = Array.from(newmessage);
+    let count = 1;
+    for (let i = 0; i < message.length; i++) {
+      if (count <= gama.length) {
+        gama[i] = gama[count - 1]
+      }
+      else {
+        count = count - gama.length;
+        gama[i] = gama[count - 1];
+      }
+      count++;
+    }
+    let newMessageNumOfSimbol = [];
+    for (let i = 0; i < newmessage.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (newmessage[i] == alphabeter[j]) {
+          newMessageNumOfSimbol[i] = j
+        };
+      }
+    }
+    let gamaNumOfSimbol = [];
+    for (let i = 0; i < gama.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (gama[i] == alphabeter[j]) {
+          gamaNumOfSimbol[i] = j
+        };
+      }
+    }
+    let difMassAndGama = [];
+    for (let i = 0; i < newmessage.length; i++) {
+
+      difMassAndGama[i] = (newMessageNumOfSimbol[i] + 1) -
+        (gamaNumOfSimbol[i] + 1);
+      if (difMassAndGama[i] < 0)
+        difMassAndGama[i] = difMassAndGama[i] + alphabeter.length;
+    }
+    for (let i = 0; i < message.length; i++) {
+      for (let j = 0; j < alphabeter.length; j++) {
+        if (difMassAndGama[i] == j + 1)
+          message[i] = alphabeter[j];
+      }
+    }
+    return message.join("");
   }
+
 }
 
 module.exports = VigenereCipheringMachine;
